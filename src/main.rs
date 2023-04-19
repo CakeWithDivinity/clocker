b1d22d112175eb90458e430f00189deca1e7ea68
hendrik.loewe <hendrikloewe3@gmail.com>
2023-04-19 21:03:22 +0200

    Add drawing loop and ability to leave tui

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
-use std::{io, thread, time::Duration};
+use std::io;
 
 use crossterm::{
+    event::{self, Event, KeyCode},
     execute,
     terminal::{enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
 use tui::{
-    backend::CrosstermBackend,
+    backend::{Backend, CrosstermBackend},
     layout::{Constraint, Direction, Layout},
     widgets::{Block, Borders},
-    Terminal,
+    Frame, Terminal,
 };
 
 fn main() -> Result<(), io::Error> {
@@ -20,34 +21,43 @@ fn main() -> Result<(), io::Error> {
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
 
-    terminal.draw(|f| {
-        let size = f.size();
+    loop {
+        terminal.draw(|f| ui(f))?;
 
-        let vert_chunks = Layout::default()
-            .direction(Direction::Horizontal)
-            .margin(1)
-            .constraints([Constraint::Percentage(40), Constraint::Percentage(60)].as_ref())
-            .split(size);
-
-        let horiz_chunks = Layout::default()
-            .direction(Direction::Vertical)
-            .constraints([Constraint::Min(1), Constraint::Max(3)].as_ref())
-            .split(vert_chunks[0]);
-
-        let block1 = Block::default().title("1").borders(Borders::ALL);
-        f.render_widget(block1, horiz_chunks[0]);
-
-        let block2 = Block::default().title("2").borders(Borders::ALL);
-        f.render_widget(block2, horiz_chunks[1]);
-
-        let block3 = Block::default().title("3").borders(Borders::ALL);
-        f.render_widget(block3, vert_chunks[1]);
-    })?;
-
-    thread::sleep(Duration::from_secs(10));
+        if let Event::Key(key) = event::read()? {
+            match key.code {
+                KeyCode::Char('q') => break,
+                _ => continue,
+            }
+        }
+    }
 
     execute!(terminal.backend_mut(), LeaveAlternateScreen)?;
     terminal.show_cursor()?;
 
     Ok(())
+}
+
+fn ui<B: Backend>(f: &mut Frame<B>) {
+    let size = f.size();
+
+    let horiz_chunks = Layout::default()
+        .direction(Direction::Horizontal)
+        .margin(1)
+        .constraints([Constraint::Percentage(40), Constraint::Percentage(60)].as_ref())
+        .split(size);
+
+    let vert_chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([Constraint::Min(1), Constraint::Max(3)].as_ref())
+        .split(horiz_chunks[0]);
+
+    let block1 = Block::default().title("1").borders(Borders::ALL);
+    f.render_widget(block1, vert_chunks[0]);
+
+    let block2 = Block::default().title("2").borders(Borders::ALL);
+    f.render_widget(block2, vert_chunks[1]);
+
+    let block3 = Block::default().title("3").borders(Borders::ALL);
+    f.render_widget(block3, horiz_chunks[1]);
 }
